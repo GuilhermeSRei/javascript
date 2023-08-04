8a78abf38ac9cc86d09dde3986ebf2960a00736c
GuilhermeSRei <139374621+GuilhermeSRei@users.noreply.github.com>
2023-08-04 15:41:26 -0300

    aulas de js

---
+++ js007/ex008.js
@@ -0,0 +1,8 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você passou da velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre com o cinto de segurança')
+
+// Condiçâo simples, pq não tem o else
