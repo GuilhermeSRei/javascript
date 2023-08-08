060c5e1b105105c5b0396e5598c4a7dbcaa22a8a
GuilhermeSRei <139374621+GuilhermeSRei@users.noreply.github.com>
2023-08-08 16:17:46 -0300

    atualizações

--- js014/ex017/script.js
+++ js014/ex017/script.js
@@ -1,26 +1,14 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML =`Agora são ${hora} horas`
+function calcular() {
+    var num = document.getElementById('num')
+    var n = Number(num.value)
+    var res = document.getElementById('res')
 
-    if (hora >= 0 && hora < 12) {
-        // BOM DIA
-        img.src = 'imagens/manha1.png'
-        msg.innerHTML += ' da mannhã'
-    } else if (hora >= 12 && hora <= 18) {
-        // BOA TARDE
-        img.src = 'imagens/tarde1.png'
-        document.body.style.backgroundImage = "linear-gradient(to right, #DE8579, #FFBD71)"
-        msg.innerHTML += ' da tarde'
+    if (num.value.length == 0) {
+        res.innerHTML = 'Adicione um número acima'
     } else {
-        // BOA NOITE
-        img.src = 'imagens/noite1.png'
-        document.body.style.backgroundImage = "linear-gradient(to right, #092958, #8093B4)"
-        msg.innerHTML += ' da noite'
-
+        do {
+            
+        } while (n >= 0)
     }
+    
 }
-
-
